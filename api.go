1357bd1dcd1c676d5b2cce40992f487645d4785c
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-13 18:29:55 -0300

    add handleAccount methods

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -49,25 +50,36 @@ func (s *APIServer) Run() {
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	// Start the server
+
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("methos not allowed %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
 
 	return nil
 }
 
-func (s *APIServer) handleGetAcoount(w http.ResponseWriter, r *http.Request) error {
-	// Start the server
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+
 	return nil
 }
 
-func (s *APIServer) handleCreateAcoount(w http.ResponseWriter, r *http.Request) error {
-	// Start the server
-	return nil
-}
-
-func (s *APIServer) handleDeleteAcoount(w http.ResponseWriter, r *http.Request) error { // Start the server
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-	// Start the server
+
 	return nil
 }

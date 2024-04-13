5729273605282bf48e3eca4e0cabd367c45e46d1
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-13 18:25:55 -0300

    add WriteJSON func

--- api.go
+++ api.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle error
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -17,29 +41,33 @@ func NewAPIServer(listenAddr string) *APIServer {
 func (s *APIServer) Run() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/account", s.handleAccount)
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	log.Println("JSON API server running on port: ", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
 }
 
-func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	// Start the server
+
+	return nil
+}
+
+func (s *APIServer) handleGetAcoount(w http.ResponseWriter, r *http.Request) error {
 	// Start the server
 	return nil
 }
 
-func (s *APIServer) handleGetAcoount(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) handleCreateAcoount(w http.ResponseWriter, r *http.Request) error {
 	// Start the server
 	return nil
 }
 
-func (s *APIServer) handleCreateAcoount(w http.ResponseWriter, r *http.Request) {
-	// Start the server
+func (s *APIServer) handleDeleteAcoount(w http.ResponseWriter, r *http.Request) error { // Start the server
 	return nil
 }
-
-func (s *APIServer) handleDeleteAcoount(w http.ResponseWriter, r *http.Request) { // Start the server
-	return nil
-}
-
-func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	// Start the server
 	return nil
 }
